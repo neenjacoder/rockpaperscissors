d0cc66a9b4b12ba225b33932b9e301f99855ca53
Cesar Hernandez <123140316+neenjacoder@users.noreply.github.com>
2023-04-20 21:57:09 -0700

    mod. button onclick based off userchoice, updated js scripts to match

--- javascript.js
+++ javascript.js
@@ -1,18 +1,65 @@
-function computerchoice () {
+function computerchoicerock() {
     var textArray = [
         "rock",
         "paper",
         "scissors",
     ];
     var randomNumber = Math.floor(Math.random()*textArray.length);
-    if (randomNumber == 0) {
-        console.log("rock");
-        return "rock";
+    if (randomNumber == 0)  {
+        console.log(randomNumber);
+        console.log("tie game");
+        return "tie game";
     } else if (randomNumber == 1) {
-        console.log("paper");
-        return "paper";
+        console.log(randomNumber);
+        console.log("player loses");
+        return "player loses";
     } else {
-        console.log("scissors");
-        return "scissors";
+        console.log(randomNumber);
+        console.log("player wins");
+        return "player wins";
+    }
+}
+
+function computerchoicescissors() {
+    var textArray = [
+        "rock",
+        "paper",
+        "scissors",
+    ];
+    var randomNumber = Math.floor(Math.random()*textArray.length);
+    if (randomNumber == 0)  {
+        console.log(randomNumber);
+        console.log("player wins");
+        return "player wins";
+    } else if (randomNumber == 1) {
+        console.log(randomNumber);
+        console.log("tie game");
+        return "tie game";
+    } else {
+        console.log(randomNumber);
+        console.log("player loses");
+        return "player loses";
+    }
+}
+
+function computerchoicepaper() {
+    var textArray = [
+        "rock",
+        "paper",
+        "scissors",
+    ];
+    var randomNumber = Math.floor(Math.random()*textArray.length);
+    if (randomNumber == 0)  {
+        console.log(randomNumber);
+        console.log("player loses");
+        return "player loses";
+    } else if (randomNumber == 1) {
+        console.log(randomNumber);
+        console.log("player wins");
+        return "player wins";
+    } else {
+        console.log(randomNumber);
+        console.log("tie game");
+        return "tie game";
     }
 }
