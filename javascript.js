13089f78e7927799b1ad9c147251402067c54774
Cesar Hernandez <123140316+neenjacoder@users.noreply.github.com>
2023-04-20 22:34:02 -0700

    mod. button onclick, mod. computergame()

--- javascript.js
+++ javascript.js
@@ -1,65 +1,96 @@
-function computerchoicerock() {
-    var textArray = [
-        "rock",
-        "paper",
-        "scissors",
-    ];
-    var randomNumber = Math.floor(Math.random()*textArray.length);
-    if (randomNumber == 0)  {
-        console.log(randomNumber);
-        console.log("tie game");
-        return "tie game";
-    } else if (randomNumber == 1) {
-        console.log(randomNumber);
-        console.log("player loses");
-        return "player loses";
-    } else {
-        console.log(randomNumber);
-        console.log("player wins");
-        return "player wins";
-    }
-}
+// function computerrock() {
+//     var textArray = [
+//         "rock",
+//         "paper",
+//         "scissors",
+//     ];
+//     var randomNumber = Math.floor(Math.random()*textArray.length);
+//     if (randomNumber == 0)  {
+//         console.log(randomNumber);
+//         console.log("tie game");
+//         return "tie game";
+//     } else if (randomNumber == 1) {
+//         console.log(randomNumber);
+//         console.log("player loses");
+//         return "player loses";
+//     } else {
+//         console.log(randomNumber);
+//         console.log("player wins");
+//         return "player wins";
+//     }
+// }
 
-function computerchoicescissors() {
-    var textArray = [
-        "rock",
-        "paper",
-        "scissors",
-    ];
-    var randomNumber = Math.floor(Math.random()*textArray.length);
-    if (randomNumber == 0)  {
-        console.log(randomNumber);
-        console.log("player wins");
-        return "player wins";
-    } else if (randomNumber == 1) {
-        console.log(randomNumber);
-        console.log("tie game");
-        return "tie game";
-    } else {
-        console.log(randomNumber);
-        console.log("player loses");
-        return "player loses";
-    }
-}
+// function computerpaper() {
+//     var textArray = [
+//         "rock",
+//         "paper",
+//         "scissors",
+//     ];
+//     var randomNumber = Math.floor(Math.random()*textArray.length);
+//     if (randomNumber == 0)  {
+//         console.log(randomNumber);
+//         console.log("player wins");
+//         return "player wins";
+//     } else if (randomNumber == 1) {
+//         console.log(randomNumber);
+//         console.log("tie game");
+//         return "tie game";
+//     } else {
+//         console.log(randomNumber);
+//         console.log("player loses");
+//         return "player loses";
+//     }
+// }
 
-function computerchoicepaper() {
+// function computerscissors() {
+//     var textArray = [
+//         "rock",
+//         "paper",
+//         "scissors",
+//     ];
+//     var randomNumber = Math.floor(Math.random()*textArray.length);
+//     if (randomNumber == 0)  {
+//         console.log(randomNumber);
+//         console.log("player loses");
+//         return "player loses";
+//     } else if (randomNumber == 1) {
+//         console.log(randomNumber);
+//         console.log("player wins");
+//         return "player wins";
+//     } else {
+//         console.log(randomNumber);
+//         console.log("tie game");
+//         return "tie game";
+//     }
+// }
+
+function computergame(userchoice) {
     var textArray = [
         "rock",
         "paper",
         "scissors",
     ];
     var randomNumber = Math.floor(Math.random()*textArray.length);
-    if (randomNumber == 0)  {
-        console.log(randomNumber);
-        console.log("player loses");
-        return "player loses";
-    } else if (randomNumber == 1) {
-        console.log(randomNumber);
-        console.log("player wins");
-        return "player wins";
-    } else {
-        console.log(randomNumber);
+    if (randomNumber == userchoice)  {
         console.log("tie game");
-        return "tie game";
+        return "user";
+    } else if (userchoice == 0 && randomNumber == 1) {
+        console.log("user chose rock comp chose paper, user loses");
+        return "computer";
+    } else if (userchoice == 0 && randomNumber == 2) {
+        console.log("user chose rock comp chose scissors, user wins");
+        return "user";
+    } else if (userchoice == 1 && randomNumber == 0) {
+        console.log("user chose paper comp chose rock, user wins");
+        return "user";
+    } else if (userchoice == 1 && randomNumber == 2) {
+        console.log("user chose paper comp chose scissors, user loses");
+        return "computer";
+    } else if (userchoice == 2 && randomNumber == 0) {
+        console.log("user chose scissors, comp chose rock, user loses");
+        return "computer";
+    } else {
+        console.log("user chose scissors, comp chose paper, user wins");
+        return "user";
     }
 }
