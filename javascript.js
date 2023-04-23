60028a595400d5fa2ff161513b0dec22ec8c554a
Cesar Hernandez <123140316+neenjacoder@users.noreply.github.com>
2023-04-23 14:07:13 -0700

    simplifying index.html, expanding functions javascript.js

--- javascript.js
+++ javascript.js
@@ -1,96 +1,56 @@
-// function computerrock() {
-//     var textArray = [
-//         "rock",
-//         "paper",
-//         "scissors",
-//     ];
-//     var randomNumber = Math.floor(Math.random()*textArray.length);
-//     if (randomNumber == 0)  {
-//         console.log(randomNumber);
-//         console.log("tie game");
-//         return "tie game";
-//     } else if (randomNumber == 1) {
-//         console.log(randomNumber);
-//         console.log("player loses");
-//         return "player loses";
-//     } else {
-//         console.log(randomNumber);
-//         console.log("player wins");
-//         return "player wins";
-//     }
-// }
-
-// function computerpaper() {
-//     var textArray = [
-//         "rock",
-//         "paper",
-//         "scissors",
-//     ];
-//     var randomNumber = Math.floor(Math.random()*textArray.length);
-//     if (randomNumber == 0)  {
-//         console.log(randomNumber);
-//         console.log("player wins");
-//         return "player wins";
-//     } else if (randomNumber == 1) {
-//         console.log(randomNumber);
-//         console.log("tie game");
-//         return "tie game";
-//     } else {
-//         console.log(randomNumber);
-//         console.log("player loses");
-//         return "player loses";
-//     }
-// }
-
-// function computerscissors() {
-//     var textArray = [
-//         "rock",
-//         "paper",
-//         "scissors",
-//     ];
-//     var randomNumber = Math.floor(Math.random()*textArray.length);
-//     if (randomNumber == 0)  {
-//         console.log(randomNumber);
-//         console.log("player loses");
-//         return "player loses";
-//     } else if (randomNumber == 1) {
-//         console.log(randomNumber);
-//         console.log("player wins");
-//         return "player wins";
-//     } else {
-//         console.log(randomNumber);
-//         console.log("tie game");
-//         return "tie game";
-//     }
-// }
-
-function computergame(userchoice) {
+// RANDOMLY GENERATES A CHOICE FOR COMPUTER (ROCK, PAPER, SCISSORS)
+function computerchoice() {
     var textArray = [
         "rock",
         "paper",
         "scissors",
     ];
-    var randomNumber = Math.floor(Math.random()*textArray.length);
-    if (randomNumber == userchoice)  {
-        // computer and user chose the same
-        return "user";
-    } else if (userchoice == 0 && randomNumber == 1) {
-        // rock vs paper, computer wins
-        return "computer";
-    } else if (userchoice == 0 && randomNumber == 2) {
-        // rock vs scissors, user wins
-        return "user";
-    } else if (userchoice == 1 && randomNumber == 0) {
-        // paper vs rock, user wins
-        return "user";
-    } else if (userchoice == 1 && randomNumber == 2) {
-        // paper vs scissors, computer wins
-        return "computer";
-    } else if (userchoice == 2 && randomNumber == 0) {
-        // scissors vs rock, computer wins
-        return "computer";
+    var computerselected = Math.floor(Math.random()*textArray.length);
+    if (computerselected == 0) {
+        return "rock";
+    } else if (computerselected == 1){
+        return "paper";
     } else {
-        // scissors vs paper, user wins
-        return "user";
+        return "scissors";
     }
 }
+
+// CHECKS TO SEE WHO THE WINNER IS BETWEEN COMPUTER OR USER FOR A SINGLE ROUND
+function roundwinner (userchoice, computerchoice) {
+    if (userchoice == computerchoice) {
+        return "tie"
+    } else if (userchoice == "rock" && computerchoice == "paper") {
+        return "computerwins";
+    } else if (userchoice == "rock" && computerchoice == "scissors") {
+        return "userwins";
+    } else if (userchoice == "paper" && computerchoice == "rock") {
+        return "userwins";
+    } else if (userchoice == "paper" && computerchoice == "scissors") {
+        return "computerwins";
+    } else if (userchoice == "scissors" && computerchoice == "rock") {
+        return "computerwins";
+    } else if (userchoice == "scissors" && computerchoice == "paper") {
+        return "userwins";
+    } else {
+        return;
+    }
+}
+
+// UPDATES THE SCORE
+function gamewinner (roundwinner) {
+    roundwinner = roundwinner();
+    user = 0;
+    computer = 0;
+    if (roundwinner == "userwins") {
+        user ++;
+    } else if (roundwinner == "computerwins") {
+        computer ++;
+    } else if (roundwinner == "tie") {
+        user += 0;
+        computer += 0;
+    } else {
+        return;
+    }
+}
+
+// CHECKS TO SEE WHO WON THE GAME
