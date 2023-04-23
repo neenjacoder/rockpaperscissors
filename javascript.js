b3e7eb98ca7884593cb71d1ff079cc72a552c207
Cesar Hernandez <123140316+neenjacoder@users.noreply.github.com>
2023-04-23 14:21:51 -0700

    mod. index.html and javascript.js clarify functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+// List of functions to play a round and check to see who is winner:
+    // 
+
 // RANDOMLY GENERATES A CHOICE FOR COMPUTER (ROCK, PAPER, SCISSORS)
 function computerchoice() {
     var textArray = [
@@ -16,10 +19,12 @@ function computerchoice() {
 }
 
 // CHECKS TO SEE WHO THE WINNER IS BETWEEN COMPUTER OR USER FOR A SINGLE ROUND
-function roundwinner (userchoice, computerchoice) {
+function playround (userchoice, computerchoice) {
+    userchoice = onclick();
+    computerchoice = computerchoice();
     if (userchoice == computerchoice) {
         return "tie"
-    } else if (userchoice == "rock" && computerchoice == "paper") {
+    } else if (userchoice == "rock" && computerchoice() == "paper") {
         return "computerwins";
     } else if (userchoice == "rock" && computerchoice == "scissors") {
         return "userwins";
@@ -37,20 +42,29 @@ function roundwinner (userchoice, computerchoice) {
 }
 
 // UPDATES THE SCORE
-function gamewinner (roundwinner) {
+function scorekeeper () {
     roundwinner = roundwinner();
-    user = 0;
-    computer = 0;
+    userscore = 0;
+    computerscore = 0;
     if (roundwinner == "userwins") {
-        user ++;
+        userscore ++;
     } else if (roundwinner == "computerwins") {
-        computer ++;
+        computerscore ++;
     } else if (roundwinner == "tie") {
-        user += 0;
-        computer += 0;
+        userscore += 0;
+        computerscore += 0;
     } else {
         return;
     }
 }
 
 // CHECKS TO SEE WHO WON THE GAME
+function gamewinner () {
+    if (userscore == 5) {
+        return "user wins";
+    } else if (computerscore == 5) {
+        return "computer wins";
+    } else {
+        return;
+    }
+}
