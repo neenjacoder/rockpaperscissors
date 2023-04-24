35aea077f338b6b9fcd0cefdd472f1d917961f84
Cesar Hernandez <123140316+neenjacoder@users.noreply.github.com>
2023-04-23 18:18:00 -0700

    mod. functions, added self update variables html

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
-// List of functions to play a round and check to see who is winner:
-    //play a round function
-        // generate a random choice for computer
-    // compare random choice computer to user choice
-    // declare a winner
-    // update score
-    // check for winner
-    // no winner keep playing, wait for input
+// Global Values
+var userscore = 0;
+var computerscore = 0;
+var winner = 5;
+var compchoicenumber = generatechoicecomputer()
+var compchoicetext = "Computer Chooses: " + choicenumbertotext()
 
 // RANDOMLY GENERATES A CHOICE FOR COMPUTER (ROCK, PAPER, SCISSORS)
 function generatechoicecomputer() {
@@ -27,74 +25,72 @@ function generatechoicecomputer() {
 // CHECKS TO SEE WHO THE WINNER IS BETWEEN COMPUTER OR USER FOR A SINGLE ROUND
 function playround(userchooses) {
     computerchooses = generatechoicecomputer();
-    userscore = 0;
-    computerscore = 0;
     if (userchooses == computerchooses) {
         console.log("tie game");
         userscore+=0;
         computerscore+=0;
-        console.log(userscore);
-        console.log(computerscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "tie";
     } else if (userchooses == 0 && computerchooses == 1) {
         console.log("computer wins");
         computerscore++;
-        console.log(computerscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "computer";
     } else if (userchooses == 0 && computerchooses == 2) {
         console.log("user wins");
         userscore++;
-        console.log(userscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "user";
     } else if (userchooses == 1 && computerchooses == 0) {
         console.log("user wins");
         userscore++;
-        console.log(userscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "user";
     } else if (userchooses == 1 && computerchooses == 2) {
         console.log("computer wins");
         computerscore++;
-        console.log(computerscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "computer";
     } else if (userchooses == 2 && computerchooses == 0) {
         console.log("computer wins");
         computerscore++;
-        console.log(computerscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "computer";
     } else if (userchooses == 2 && computerchooses == 1) {
         console.log("user wins");
         userscore++;
-        console.log(userscore);
+        console.log("User Score: " + userscore);
+        console.log("Computer Score: " + computerscore);
         return "user";
     } else {
         return;
     }
 }
 
-// // UPDATES THE SCORE
-// function scorekeeper () {
-//     roundwinner = roundwinner();
-//     userscore = 0;
-//     computerscore = 0;
-//     if (roundwinner == "userwins") {
-//         userscore ++;
-//     } else if (roundwinner == "computerwins") {
-//         computerscore ++;
-//     } else if (roundwinner == "tie") {
-//         userscore += 0;
-//         computerscore += 0;
-//     } else {
-//         return;
-//     }
-// }
+function gameover() {
+    if (userscore == winner) {
+        return "user wins";
+    } else if (computerscore == winner) {
+        return "computer wins";
+    } else {
+        return;
+    }
+}
 
-// // CHECKS TO SEE WHO WON THE GAME
-// function gamewinner () {
-//     if (userscore == 5) {
-//         return "user wins";
-//     } else if (computerscore == 5) {
-//         return "computer wins";
-//     } else {
-//         return;
-//     }
-// }
+function choicenumbertotext() {
+    if (compchoicenumber == 0) {
+        return "rock";
+    } else if (compchoicenumber == 1) {
+        return "paper";
+    } else if (compchoicenumber == 2) {
+        return "scissors";
+    } else {
+        return;
+    }
+}
