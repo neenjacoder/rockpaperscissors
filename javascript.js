6e654d31bddc3564e1afc5c56acc7cdc37e7f4b5
Cesar Hernandez <123140316+neenjacoder@users.noreply.github.com>
2023-05-04 22:48:06 -0700

    created functions

--- javascript.js
+++ javascript.js
@@ -1,23 +1,16 @@
-var computerscore = 0;
-var usersscore = 0;
+var computerpoints = 0;
+var userpoints = 0;
 var winner = 5;
 
 function playGame(userchoice) {
-    computer = randomComputerChoice();
-    user = userchoice;
-     if (user = computer) {
-        userscore+=0;
-        computerscore+=0;
+    if (userchoice == "rock") {
+        userchoserock();
         return;
-    } else if ((user == "rock" && computer == "paper") || (user == "paper" && computer == "scissors") || (user == "scissors" && computer == "rock")) {
-        computerscore+=1;
-        document.getElementById("computerscore").innerText = computerscore;
-        return;
-    } else if ((user == "rock" && computer == "scissors") || (user == "paper" && computer == "rock") || (user == "scissors" && computer == "paper")) {
-        userscore+=1;
-        document.getElementById("userscore").innerText = userscore;
+    } else if (userchoice == "paper") {
+        userchosepaper();
         return;
     } else {
+        userchosescissors();
         return;
     }
 }
@@ -37,3 +30,88 @@ function randomComputerChoice() {
 
 }
 
+function userchoserock() {
+    computer = randomComputerChoice();
+    console.log("User chose rock")
+    console.log("Computer chose " + computer);
+    if (computer == "rock") {
+        document.getElementById("winneris").innerText = "This Round Was A Tie!";
+        console.log("Tie")
+    } else if (computer == "scissors") {
+        userpoints++;
+        document.getElementById("userscore").innerText = "User Score: " + userpoints;
+        document.getElementById("winneris").innerText = "User Wins This Round!";
+        console.log("User wins")
+        checkwinner();
+        resetgame();
+    } else {
+        computerpoints++;
+        document.getElementById("computerscore").innerText = "Computer Score: " + computerpoints;
+        document.getElementById("winneris").innerText = "Computer Wins This Round!";
+        console.log("Computer wins")
+        checkwinner();
+        resetgame(); 
+    }
+}
+
+function userchosepaper() {    
+    computer = randomComputerChoice();
+    console.log("User chose paper")
+    console.log("Computer chose " + computer);
+    if (computer == "rock") {
+        userpoints++;
+        document.getElementById("userscore").innerText = "User Score: " + userpoints;
+        document.getElementById("winneris").innerText = "User Wins This Round!";
+        console.log("User wins")
+        checkwinner();
+        resetgame();    
+    } else if (computer == "scissors") {
+        computerpoints++;
+        document.getElementById("computerscore").innerText = "Computer Score: " + computerpoints;
+        document.getElementById("winneris").innerText = "Computer Wins This Round!";
+        console.log("Computer wins")
+        checkwinner();
+        resetgame(); 
+    } else {
+        document.getElementById("winneris").innerText = "This Round Was A Tie!";
+        console.log("Tie")
+    }
+}
+
+function userchosescissors() {
+    computer = randomComputerChoice();
+    console.log("User chose scissors");
+    console.log("Computer chose " + computer);
+    if (computer == "rock") {
+        computerpoints++;
+        document.getElementById("computerscore").innerText = "Computer Score: " + computerpoints;
+        document.getElementById("winneris").innerText = "Computer Wins This Round!";
+        console.log("Computer wins")
+        checkwinner();
+        resetgame();  
+    } else if (computer == "scissors") {
+        document.getElementById("winneris").innerText = "This Round Was A Tie!";
+        console.log("Tie")
+    } else {
+        userpoints++;
+        document.getElementById("userscore").innerText = "User Score: " + userpoints;
+        document.getElementById("winneris").innerText = "User Wins This Round!";
+        console.log("User wins")
+        checkwinner();
+        resetgame();        
+    }
+}
+
+function checkwinner() {
+    if (userpoints == winner) {
+        document.getElementById("winneris").innerText = "Congrats You Win!";
+        return;
+    } else if (computerpoints == winner) {
+        document.getElementById("winneris").innerText = "Disaster, The Computer Won!";
+        return;
+    } else {
+        return;
+    }
+}
+
+// create a function to reset the game
